c32694f744d5a02ee2de30ba8ae1aca35e8f530e
squidserv <mprovost1992@gmail.com>
2021-09-30 18:23:40 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Analgesic Bullet
+
+Shoot the drunk and the magic bullet will instantly sober them up instantly! Be careful though, you may remind them that they are your enemy!
+
+## Convars
+
+Add the following to your server.cfg (for dedicated servers) or listenserver.cfg (for peer-to-peer servers):
+
+```cpp
+ttt_analgesic_random_team                   0.50    // The chance that the drunk will join the team that uses it
+ttt_analgesic_jester_use                    0       // Whether the gun does anything if a Jester Team uses it
+ttt_analgesic_not_drunk                     1       // How to handle if the gun is used on someone who isn't drunk
+
+// ----------------------------------------
+// Not Drunk Modes
+// ----------------------------------------
+// 0    -   Does nothing
+// 1    -   Kills the target
+// 2    -   Kills the user
+// 3    -   Heals the target
+```
+
+## WARNING
+
+You must be using Custom Roles version 1.2.7 for this mod to work.
+
+## Special Thanks:
+- [Noxx](https://steamcommunity.com/id/noxxflame) and [Malivil](https://steamcommunity.com/id/malivil) for all their work on Custom Roles for TTT
+- [Ianthina](https://steamcommunity.com/id/2ahlamfatin) for the idea
---
+++ lua/weapons/weapon_analgesic.lua
@@ -0,0 +1,131 @@
+if SERVER then
+    AddCSLuaFile()
+end
+
+if CLIENT then
+    SWEP.PrintName       = "Analgesic Bullet"
+    SWEP.Author			= "Squid Matty"
+    SWEP.Contact			= "https://steamcommunity.com/id/mattyp92/";
+    SWEP.Instructions	= "Shoot the drunk to sober them up"
+    SWEP.Slot = 0
+    SWEP.SlotPos = 1
+    SWEP.IconLetter		= "M"
+end
+
+SWEP.Base = "weapon_tttbase"
+SWEP.InLoadoutFor = nil
+SWEP.AllowDrop = true
+SWEP.IsSilent = false
+SWEP.NoSights = false
+SWEP.LimitedStock = true
+
+SWEP.Spawnable = true
+SWEP.AdminSpawnable = true
+SWEP.AdminOnly = false
+SWEP.DrawAmmo = false
+SWEP.DrawCrosshair = true
+
+SWEP.HoldType		= "pistol"
+SWEP.UseHands = true
+SWEP.ViewModel  = "models/weapons/v_pist_deagle.mdl"
+SWEP.WorldModel = "models/weapons/w_pist_deagle.mdl"
+SWEP.Kind = WEAPON_EQUIP1
+SWEP.CanBuy = { ROLE_DETECTIVE, ROLE_TRAITOR }
+SWEP.AutoSpawnable = false
+
+SWEP.Primary.ClipSize		= 1
+SWEP.Primary.DefaultClip	= 1
+SWEP.Primary.Automatic		= false
+SWEP.Primary.Ammo		= "Analgesic"
+
+SWEP.Weight					= 7
+
+local randomChance = CreateConVar( "ttt_analgesic_random_team", 0.50 , {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "The chance the drunk joins the user's team" )
+local nonDrunk = CreateConVar( "ttt_analgesic_not_drunk", 1 , {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "How it should handle being used on non-drunks" )
+local jesterUse = CreateConVar( "ttt_analgesic_jester_use", 0 , {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "Can Jester's use it?" )
+
+
+function SWEP:SecondaryAttack()
+    if not self.IronSightsPos then return end
+    if self:GetNextSecondaryFire() > CurTime() then return end
+
+    local bIronsights = not self:GetIronsights()
+
+    self:SetIronsights( bIronsights )
+
+    self:SetZoom( bIronsights )
+
+    self:SetNextSecondaryFire( CurTime() + 0.3 )
+end
+
+function SWEP:PrimaryAttack()
+    local p = self.Owner
+    if self:Clip1() <= 0 or not CR_VERSION or not CRVersion("1.2.7") then return end
+
+    self:SendWeaponAnim(self.PrimaryAnim)
+    self.Owner:MuzzleFlash()
+    self.Owner:SetAnimation( PLAYER_ATTACK1 )
+
+    local Bullet = {}
+
+    Bullet.Dmgtype = "DMG_GENERIC"
+    Bullet.Num = num
+    Bullet.Spread = Vector( cone, cone, 0 )
+    Bullet.Tracer = 0
+    Bullet.Force = 0
+    Bullet.Damage = 0
+    Bullet.Src = p:GetShootPos()
+    Bullet.Dir = p:GetAimVector()
+    Bullet.TracerName = "TRACER_NONE"
+
+    Bullet.Callback = function(atk, tr, dmg)
+        local tgt = tr.Entity
+        if SERVER then
+            if not tgt:IsPlayer() or tgt:IsAlive() then return end
+            local team = p:GetRoleTeam(true)
+            if tgt:IsDrunk() then
+                if team ~= ROLE_TEAM_JESTER then
+                    if math.random() <= randomChance then
+                        tgt:SoberDrunk(p:GetRoleTeam(true))
+                    else
+                        tgt:SoberDrunk()
+                    end
+                elseif jesterUse then
+                    tgt:SoberDrunk()
+                end
+            elseif nonDrunk == 1 then
+                tgt:Kill()
+            elseif nonDrunk == 2 then
+                p:Kill()
+            elseif nonDrunk == 3 then
+                tgt:SetHealth(tgt:GetMaxHealth())
+            end
+        end
+    end
+    self:TakePrimaryAmmo( 1 )
+    p:FireBullets( Bullet )
+end
+
+function SWEP:Holster()
+    self:SetIronsights(false)
+    self:SetZoom(false)
+    return true
+end
+
+function SWEP:PreDrop()
+    self:SetZoom(false)
+    self:SetIronsights(false)
+    return self.BaseClass.PreDrop(self)
+end
+
+function SWEP:Reload()
+    self.Weapon:DefaultReload( ACT_VM_RELOAD );
+    self:SetIronsights( false )
+    self:SetZoom(false)
+end
+
+function SWEP:WasBought(buyer)
+    if IsValid(buyer) then -- probably already self.Owner
+        buyer:GiveAmmo( 1, "Analgesic", true )
+    end
+end
